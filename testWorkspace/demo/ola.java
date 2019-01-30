f8b424e981ca44e2f24561347f27bb632e5bbcc2
BernardoFLRibeiro <38184444+BernardoFLRibeiro@users.noreply.github.com>
2019-01-30 18:01:25 +0000

    Add files via upload

---
+++ testWorkspace/demo/ola.java
@@ -0,0 +1,50 @@
+package pt.iscte.pidesco.demo.ext;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+import pt.iscte.pidesco.demo.extensibility.DemoAction;
+
+public class ola {
+
+	public ola(){
+		run();
+	}
+
+	void run() {
+		
+	}
+
+	public void testArray() {
+
+		int[] array = new int[2];
+		int vetor2[];
+
+		int[] vetor3 = { 1, 2, 3 };
+
+
+	int third = vetor3[2];
+
+	
+	int j=9;
+	String ola = "ola";
+	String h = ola.toString();
+
+	boolean loading = true;
+	
+	if(loading){
+
+	}
+	
+	boolean verdadeiro = true;
+
+	while(verdadeiro){
+
+	}
+
+	}
+}
+
+
+
